bc80342ce65c8738c292c9a2cd119b368c93ef7d
Thomas Weise <tweise@hfuu.edu.cn>
2026-03-19 07:42:34 +0800

    added zip compression script

--- linux/zipCompress.sh
+++ linux/zipCompress.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Compress files and folders to .tar.xz archives, using the strongest
-# available compression.
+# Compress files and folders to .zip archives, using the strongest
+# available zip compression.
+# This compression is generally worse than tar.xz compression.
 # Later, you can decompress the generated archive with the command
-# "tar -xf archive.tar.xz".
+# "unzip archive.zip".
 #
 # The script can be called in two ways:
 #
 # 1. With a single parameter 'X', which can be either a file or directory.
-#    Then, an archive with name 'X.tar.xz' is created and the contents of 'X'
+#    Then, an archive with name 'X.zip' is created and the contents of 'X'
 #    are packaged into it.
 #
 # 2. With multiple parameters 'Y', 'A', 'B', 'C', and so on.
-#    Then, an archive with name 'Y.tar.xz' is created and the contents of 'A',
+#    Then, an archive with name 'Y.zip' is created and the contents of 'A',
 #    'B', and 'C', and so on are packaged into it.
 #    'Y' is treated solely as archive name, not as source.
-#
-# This script may take a lot of memory and time.
-# If you have N logical CPU cores, this script attempts to use
-# max{1, ((N-1)/2)-1} threads and launches the compressor with niceness of 19.
-# Therefore, the system should still be usable during compression.
 
 # strict error handling
 set -o pipefail  # trace ERR through pipes
@@ -28,29 +24,16 @@ set -o nounset   # set -u : exit the script if you try to use an uninitialized v
 set -o errexit   # set -e : exit the script if any statement returns a non-true return value
 
 dest="${1%/}"
-dest="$(basename "$dest").tar.xz"
+dest="$(basename "$dest").zip"
 echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Destination archive name is '$dest'."
-
-if command -v nproc &> /dev/null; then
-    nthreads="$(nproc --all)"
-    nthreads="$((nthreads - 1))"
-    nthreads="$((nthreads / 2))"
-    nthreads="$((nthreads - 1))"
-    if [ $nthreads -le 1 ]; then
-        nthreads=1
-    fi
-else
-    nthreads=1
-fi
-echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Using $nthreads threads."
 
 if [ $# \> 1 ];
 then
     echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Beginning to compress sources '${@:2}' to destination '$dest'"
-    nice -n 19 tar -c "${@:2}" | xz --threads=$nthreads -v -9e -c > "$dest"
+    nice -n 19 zip -9 -r "$dest" "${@:2}"
 else
     echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Beginning to compress sources '$1' to destination '$dest'"
-    nice -n 19 tar -c "$1" | xz --threads=$nthreads -v -9e -c > "$dest"
+    nice -n 19 zip -9 -r "$dest" "$1"
 fi
 
 echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Done compressing to destination '$dest'."
