7e0efc1dd295cd33265ebb14af9d3edf472b57c8
Thomas Weise <tweise@hfuu.edu.cn>
2026-03-20 07:04:04 +0800

    added office2pdf script

---
+++ linux/office2pdf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -
+
+# Convert an MS Office document (doc, docx, xls, xlsx, ppt, pptx, ...) to pdf.
+# The script expects a path to an MS Office file as input.
+# It will create a document with the same name but .pdf as extension in the
+# current directory.
+# The conversion may not preserve some images correctly, but it more or less
+# works.
+
+# strict error handling
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
+set -o errexit   # set -e : exit the script if any statement returns a non-true return value
+
+srcDocument="$1"
+dstDocument="$(basename "${srcDocument%.*}.pdf")"
+echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Converting '$srcDocument' to a PDF."
+
+package="libreoffice"
+if dpkg-query -W -f='${Status}' "$package" 2>/dev/null | grep -q "ok installed"; then
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): LibreOffice is installed, so we can use it."
+else
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): LibreOffice is not installed. We install it now. This needs to be done only once."
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We first update the package cache. This requires sudo privileges."
+  sudo apt-get update -y
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): We now install LibreOffice. This requires sudo privileges."
+  sudo apt-get install -y libreoffice
+  echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Installation is finished."
+fi
+
+echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Now converting '$srcDocument' to '$dstDocument'."
+libreoffice --headless --safe-mode --convert-to pdf "$srcDocument" --outdir .
+echo "$(date +'%0Y-%0m-%0d %0R:%0S'): Finished converting '$srcDocument' to '$dstDocument'."
